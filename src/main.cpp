086fe3761893648221ba288085ce726de739029a
Vadim Safonov <vsafonov@ptsecurity.com>
2017-09-19 00:56:52 +0700

    [+] CMake example

---
+++ src/main.cpp
@@ -0,0 +1,13 @@
+#include <vector>
+#include <iostream>
+#include "bin_search.h"
+
+int main()
+{
+    std::vector<int> vec{1, 17, -10, 0};
+    sort(vec);
+    int *p1 = bin_search(vec, 16);
+    std::cout << "should be true: " << (p1 == nullptr) << std::endl;
+    int *p2 = bin_search(vec, 17);
+    std::cout << "should be true: " << (p2 == &vec[4]) << std::endl;
+}
